e3cdfa717864e360598533a4d1cc5aa168ddd3fb
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-09-09 16:27:09 -0300

    criando um stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,35 +19,52 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(
-                color: Colors.amberAccent,
-                height: 140,
-              ),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        color: Colors.black26,
-                        width: 72,
-                        height: 100,
-                      ),
-                      Text('Aprender flutter'),
-                      ElevatedButton(
-                          onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-                    ]),
-              )
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task(),
+            Task(),
+            Task(),
+            Task(),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
         ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  const Task({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.amberAccent,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Text('Aprender flutter'),
+                  ElevatedButton(
+                      onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                ]),
+          )
+        ],
       ),
     );
   }
