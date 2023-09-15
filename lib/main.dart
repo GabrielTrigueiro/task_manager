f5e2f984847364872ff0c5a063f2768c88770aae
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-09-15 09:00:41 -0300

    widget de foto

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,16 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Task('Testando'),
-            Task('titulo um'),
-            Task('titulo dois'),
-            Task('titulo tres'),
+            Task('Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task('Andar de Bike',
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
+            Task('Meditar',
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task('Ler',
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+            Task('Jogar',
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg'),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -39,7 +45,8 @@ class MyApp extends StatelessWidget {
 // esse componente não vai atualizar o nivel pois é stateless
 class Task extends StatefulWidget {
   final String titulo;
-  const Task(this.titulo, {Key? key}) : super(key: key);
+  final String foto;
+  const Task(this.titulo, this.foto, {Key? key}) : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
@@ -70,6 +77,7 @@ class _TaskState extends State<Task> {
                         color: Colors.black26,
                         width: 72,
                         height: 100,
+                        child: Image.network(widget.foto, fit: BoxFit.cover),
                       ),
                       //dando largura para o container o texto nao ultrapassa ele
                       Container(
