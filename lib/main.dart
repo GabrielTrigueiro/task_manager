c440052640883efc4946e19cecb56a0ee2ec37d3
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-09-25 18:11:37 -0300

    ajeitando

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,31 @@
+import 'package:alura_flutter_curso_1/screens/tela_inicial.dart';
 import 'package:flutter/material.dart';
-import 'package:task_manager/components/Task.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  bool opacidade = true;
   // This widget is the root of your application.
   @override
+  bool opacidade = true;
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Container(),
-          title: Text('Tarefas'),
-        ),
-        body: AnimatedOpacity(
-          opacity: opacidade ? 1 : 0,
-          duration: Duration(milliseconds: 300),
-          child: ListView(
-            children: [
-              Task(
-                  'Aprender Flutter',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  2),
-              Task(
-                  'Andar de Bike',
-                  'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-                  3),
-              Task(
-                  'Meditar',
-                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                  4),
-              Task(
-                  'Ler',
-                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-                  2),
-              Task('Jogar',
-                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 1),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              opacidade = !opacidade;
-            });
-          },
-          child: opacidade
-              ? Icon(Icons.remove_red_eye)
-              : Icon(Icons.panorama_fish_eye),
-        ),
-      ),
+      home: InitialScreen(),
     );
   }
 }
+
+
+
