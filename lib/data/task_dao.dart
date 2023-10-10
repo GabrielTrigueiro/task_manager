15206dda383f95416cbe1da5e067475007656126
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-10-10 12:38:45 -0300

    criando funcionalidade de adicionar tarefa e editar

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -13,8 +13,8 @@ class TaskDao {
   static const String _difficulty = "difficulty";
   static const String _image = 'image';
 
+  //função que transforma Map para List<Task>
   List<Task> transformMapToList(List<Map<String, dynamic>> mapaDeTarefas) {
-    //função que transforma Map para List<Task>
     final List<Task> tarefas = [];
     for (Map<String, dynamic> linha in mapaDeTarefas) {
       final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
@@ -23,7 +23,34 @@ class TaskDao {
     return tarefas;
   }
 
-  save(Task tarefa) async {}
+  //função que transforma uma tarefa em uma linha da tabela
+  Map<String, dynamic> taskToLine(Task tarefa) {
+    final Map<String, dynamic> tarefaMapeada = Map();
+    tarefaMapeada[_name] = tarefa.nome;
+    tarefaMapeada[_image] = tarefa.foto;
+    tarefaMapeada[_difficulty] = tarefa.dificuldade;
+    return tarefaMapeada;
+  }
+
+  //CRUD
+  save(Task tarefa) async {
+    final Database bancoDeDados = await getDatabase();
+
+    var itemExist = await find(tarefa.nome);
+
+    if (itemExist.isEmpty) {
+      print("De boa pae");
+      return await bancoDeDados.insert(_tablename, taskToLine(tarefa));
+    }
+
+    //caso a tarefa já exista, atualiza ela com os valores recebidos
+    else {
+      print("Tarefa já existe pae");
+      return await bancoDeDados.update(_tablename, taskToLine(tarefa),
+          where: "$_name = ?", whereArgs: [tarefa.nome]);
+    }
+  }
+
   Future<List<Task>> findAll() async {
     final Database bancoDeDados = await getDatabase(); //pega o banco de dados
 
@@ -35,9 +62,16 @@ class TaskDao {
     return transformMapToList(result);
   }
 
-  // Future<List<Task>> find() async {
-  //   print("Acessando find");
-  // }
+  Future<List<Task>> find(String nomeDaTarefa) async {
+    final Database bancoDeDados = await getDatabase(); //pega o banco de dados
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tablename, //pesquisa na tabela
+            where: "$_name = ?", //pesquisa no campo nome
+            whereArgs: [nomeDaTarefa] //valores iguais a esse
+            );
+    return transformMapToList(result);
+  }
+
   // delete(String nomeDaTarefa) async {
   //   print("Acessando delete");
   // }
