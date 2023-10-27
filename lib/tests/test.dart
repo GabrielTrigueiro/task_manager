2fc9b58b8baa135b4eba96982c3feff34578b628
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-10-27 18:25:48 -0300

    criando finder

--- lib/tests/test.dart
+++ lib/tests/test.dart
@@ -2,11 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:task_manager/tests/MyWidget.dart';
 
 main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows you to build and interact
-  // with widgets in the test environment.
   testWidgets('MyWidget has a title and message', (tester) async {
     //crio o widget que quero testar aqui dentro e passo seus parametros utilizando pumpWidget()
     await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
+
+    //criando finders com o valor e tipo que esperamos
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
   });
 }
