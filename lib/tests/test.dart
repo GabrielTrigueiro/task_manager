7245a6a0ae20ef216ee715d35dc3589afce18aa0
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-10-27 18:16:23 -0300

    usando pumpWidget para construir o widget a ser testado

--- lib/tests/test.dart
+++ lib/tests/test.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:task_manager/tests/MyWidget.dart';
 
 main() {
   // Define a test. The TestWidgets function also provides a WidgetTester
   // to work with. The WidgetTester allows you to build and interact
   // with widgets in the test environment.
   testWidgets('MyWidget has a title and message', (tester) async {
-    // Test code goes here.
+    //crio o widget que quero testar aqui dentro e passo seus parametros utilizando pumpWidget()
+    await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
   });
 }
