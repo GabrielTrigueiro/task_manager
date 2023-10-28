714941f12c6fc93ea86c330e4d912c2201563109
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-10-28 09:36:26 -0300

    teste funcionando

--- lib/tests/test.dart
+++ lib/tests/test.dart
@@ -3,11 +3,18 @@ import 'package:task_manager/tests/MyWidget.dart';
 
 main() {
   testWidgets('MyWidget has a title and message', (tester) async {
-    //crio o widget que quero testar aqui dentro e passo seus parametros utilizando pumpWidget()
     await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
-
-    //criando finders com o valor e tipo que esperamos
     final titleFinder = find.text('T');
     final messageFinder = find.text('M');
+
+    //usando findsOneWidget para verificar se os textos aparecem como o esperado
+    //obs:
+    // findsNothing - Verifies that no widgets are found.
+    // findsWidgets - Verifies that one or more widgets are found.
+    // findsNWidgets - Verifies that a specific number of widgets are found.
+    // matchesGoldenFile - Verifies that a widget’s rendering matches a particular bitmap image (“golden file” testing).
+
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
   });
 }
